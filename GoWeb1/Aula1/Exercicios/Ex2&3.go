e9593b2715045f2b6b7eb4d385f28190b9cbb41b
PedroHODL <pedrohenrique.lopes@mercadolivre.com>
2022-05-30 14:50:36 -0300

    Refactor

--- GoWeb1/Aula1/Exercicios/Ex2&3.go
+++ GoWeb1/Aula1/Exercicios/Ex2&3.go
@@ -2,21 +2,15 @@ package Exercicios
 
 import (
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
 func Ex2_3() {
 	gin.SetMode("release")
 	router := gin.Default()
 
-	arq, err := os.ReadFile("./GoWeb1/Aula1/message.json")
-	if err != nil {
-		panic(err)
-	}
-
 	//Ex2
 	router.GET("/", func(c *gin.Context) {
-		c.Data(200, "json", arq)
+		c.File("./GoWeb1/Aula1/message.json")
 	})
 
 	//Ex3
@@ -29,9 +23,5 @@ func Ex2_3() {
 }
 
 func GetAll(c *gin.Context) {
-	arq, err := os.ReadFile("./GoWeb1/Aula1/products.json")
-	if err != nil {
-		panic(err)
-	}
-	c.Data(200, "json", arq)
+	c.File("./GoWeb1/Aula1/products.json")
 }

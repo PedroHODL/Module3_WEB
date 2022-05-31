197e149040df1f4c83f0646353aead925770bd9c
PedroHODL <pedrohenrique.lopes@mercadolivre.com>
2022-05-31 17:51:14 -0300

    Adicionado services.go

--- GoWeb2&3-Arquitecture/internal/produto/services.go
+++ GoWeb2&3-Arquitecture/internal/produto/services.go
@@ -1,1 +1,26 @@
-package produto
+package blabla
+
+import (
+	"github.com/GoWeb2&3-Arquitecture/internal/produto/repository"
+)
+
+type Services interface {
+	GetAll() ([]Product, error)
+	Create(id int, name, productType string, count int, price float64) (Product, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func newService(r Repository) Services {
+	s := service{r}
+	return &s
+}
+
+func (s *service) GetAll() ([]Product, error) {
+	ps, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+}

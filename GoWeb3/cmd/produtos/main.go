e77ac8d681239d287d9441d6bd91fd1fa63f231d
PedroHODL <pedrohenrique.lopes@mercadolivre.com>
2022-06-01 18:16:45 -0300

    Adicionado persistência de dados File

--- GoWeb3/cmd/produtos/main.go
+++ GoWeb3/cmd/produtos/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/PedroHODL/Module3_WEB.git/GoWeb3/cmd/produtos/handler"
 	"github.com/PedroHODL/Module3_WEB.git/GoWeb3/internal/produto"
+	"github.com/PedroHODL/Module3_WEB.git/GoWeb3/pkg/store"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -11,7 +12,9 @@ func main() {
 	gin.SetMode("release")
 	_ = godotenv.Load("./.env")
 
-	rep := produto.NewRepository()
+	db := store.New(store.FileType, "./produtos.json")
+
+	rep := produto.NewRepository(db)
 	service := produto.NewService(rep)
 	p := handler.NewProduct(service)
 
--- GoWeb3/internal/produto/repository.go
+++ GoWeb3/internal/produto/repository.go
@@ -3,6 +3,8 @@ package produto
 import (
 	"errors"
 	"fmt"
+
+	"github.com/PedroHODL/Module3_WEB.git/GoWeb3/pkg/store"
 )
 
 type Product struct {
@@ -12,9 +14,6 @@ type Product struct {
 	Count       int     `json:"count"`
 	Price       float64 `json:"price"`
 }
-
-var ListProdutos []Product
-
 type Repository interface {
 	GetAll() ([]Product, error)
 	Create(id int, name, productType string, count int, price float64) (Product, error)
@@ -24,55 +23,79 @@ type Repository interface {
 	DeleteProduct(id int) error
 }
 
-type repository struct{}
-
-func NewRepository() Repository {
-	return &repository{}
+type repository struct {
+	db store.Store
 }
 
-func (repository) GetAll() ([]Product, error) {
+func NewRepository(db store.Store) Repository {
+	return &repository{db: db}
+}
+
+func (r repository) GetAll() ([]Product, error) {
+	var ListProdutos []Product
+	r.db.Read(&ListProdutos)
 	return ListProdutos, nil
 }
 
-func (repository) Create(id int, name, productType string, count int, price float64) (Product, error) {
+func (r repository) Create(id int, name, productType string, count int, price float64) (Product, error) {
+	var ListProdutos []Product
+	r.db.Read(&ListProdutos)
+
 	p := Product{id, name, productType, count, price}
 	ListProdutos = append(ListProdutos, p)
+
+	r.db.Write(ListProdutos)
 	return p, nil
 }
 
-func (repository) LastID() int {
-	var id int = 1
+func (r repository) LastID() int {
+	var ListProdutos []Product
+	r.db.Read(&ListProdutos)
+
+	var id int = 0
 	if len(ListProdutos) > 0 {
 		id = ListProdutos[len(ListProdutos)-1].ID + 1
 	}
 	return id
 }
 
-func (repository) Update(id int, name, productType string, count int, price float64) (Product, error) {
+func (r repository) Update(id int, name, productType string, count int, price float64) (Product, error) {
+	var ListProdutos []Product
+	r.db.Read(&ListProdutos)
+
 	p := Product{id, name, productType, count, price}
 	for i := range ListProdutos {
 		if ListProdutos[i].ID == p.ID {
 			ListProdutos[i] = p
+			r.db.Write(ListProdutos)
 			return ListProdutos[i], nil
 		}
 	}
 	return Product{}, errors.New("ID não encontrado")
 }
 
-func (repository) UpdateName(id int, name string) (Product, error) {
+func (r repository) UpdateName(id int, name string) (Product, error) {
+	var ListProdutos []Product
+	r.db.Read(&ListProdutos)
+
 	for i := range ListProdutos {
 		if ListProdutos[i].ID == id {
 			ListProdutos[i].Name = name
+			r.db.Write(ListProdutos)
 			return ListProdutos[i], nil
 		}
 	}
 	return Product{}, fmt.Errorf("produto %d não encontrado", id)
 }
 
-func (repository) DeleteProduct(id int) error {
+func (r repository) DeleteProduct(id int) error {
+	var ListProdutos []Product
+	r.db.Read(&ListProdutos)
+
 	for i := range ListProdutos {
 		if ListProdutos[i].ID == id {
 			ListProdutos = append(ListProdutos[:i], ListProdutos[i+1:]...)
+			r.db.Write(ListProdutos)
 			return nil
 		}
 	}
